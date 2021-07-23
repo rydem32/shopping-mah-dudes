22bedede72dbf4b687d376703c210dccb93b5435
rydem32 <87016997+rydem32@users.noreply.github.com>
2021-07-23 13:59:29 -0400

    lookup product identity

--- shopping_cart.py
+++ shopping_cart.py
@@ -42,6 +42,6 @@ def to_usd(my_price):
 #info capture (user input)
 
 selected_id = input("Please input a Product Identifier: ")
-print(selected_id)
-print(type(selected_id))
-
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+matching_product = matching_products[0]
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
