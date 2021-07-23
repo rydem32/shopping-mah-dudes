47bc0d5ae6968a654e522b95000e5a37e0a502ae
rydem32 <87016997+rydem32@users.noreply.github.com>
2021-07-23 14:36:43 -0400

    shifting receipt to the end

--- shopping_cart.py
+++ shopping_cart.py
@@ -41,7 +41,28 @@ def to_usd(my_price):
 
 #info capture (user input)
 
-selected_id = input("Please input a Product Identifier: ")
-matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-matching_product = matching_products[0]
-print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+total_price = 0
+selected_ids = []
+
+while True:
+        selected_id = input("Please input a Product Identifier: ")
+        
+        if selected_id == "DONE":
+                break
+        else:
+            #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+            #matching_product = matching_products[0]
+            #total_price = total_price + matching_product["price"]
+            #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+            selected_ids.append(selected_id)
+
+
+#info display / output
+
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+print("TOTAL PRICE: " + str(total_price))
